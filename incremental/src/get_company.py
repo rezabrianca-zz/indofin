ca1fd6624ab78addd62feab4c914fc417410f3fb
Reza <reza.brianca@gmail.com>
2018-11-03 14:47:58 +0700

    change message

--- incremental/src/get_company.py
+++ incremental/src/get_company.py
@@ -68,8 +68,8 @@ try:
 
     # store in csv
     company_df.to_csv('../data/raw/kode_saham_{0}.csv'.format(today), index=False)
-    print('Process completed. There are {0} public companies at {1}'.format(company_df.shape[0], today))
-    sendMessage('Process completed. There are {0} public companies at {1}'.format(company_df.shape[0], today))
+    print('There are {0} public companies at {1}'.format(company_df.shape[0], today))
+    sendMessage('Get company script completed. There are {0} public companies at {1}'.format(company_df.shape[0], today))
 
 finally:
     # close the browser
