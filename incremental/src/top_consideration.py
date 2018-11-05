b4588e767ba305cce9452b849cd6a7706005fd20
Reza <reza.brianca@gmail.com>
2018-11-05 10:41:38 +0700

    add last price info

--- incremental/src/top_consideration.py
+++ incremental/src/top_consideration.py
@@ -4,6 +4,7 @@ import os
 
 os.chdir('/home/ubuntu/indofin/incremental/src/')
 source_path = '../data/raw/stock_data/'
+apikey = os.getenv('API_KEY')
 
 today = pd.to_datetime('today').strftime('%Y-%m-%d')
 stock_df = pd.read_csv('../data/raw/kode_saham_{0}.csv'.format(today))[['Kode', 'Nama']]
@@ -19,6 +20,7 @@ selected_df = joined_df[joined_df['profit_growth_median'] > low_threshold]
 s_list = []
 s_mean = []
 s_median = []
+last_price = []
 
 print('Begin get top consideration company.')
 for f in os.listdir(source_path):
@@ -26,20 +28,23 @@ for f in os.listdir(source_path):
         s_data = pd.read_csv(source_path + f)
         s_change_mean = s_data.percent_gain.describe()[1]
         s_change_median = s_data.percent_gain.describe()[5]
+        s_price = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={0}.JK&apikey={1}&datatype=csv'.format(f.split('.')[0], apikey), usecols=['timestamp', 'adjusted_close'])
         s_list.append(f.split('.')[0])
         s_mean.append(s_change_mean)
         s_median.append(s_change_median)
+        last_price.append(s_price.adjusted_close[0])
 
 stock_data = pd.DataFrame.from_dict({
             'stock_label':s_list,
             'stock_growth_mean':s_mean,
-            'stock_growth_median':s_median
+            'stock_growth_median':s_median,
+            'last_price':last_price
             })
 
 complete_df = pd.merge(selected_df, stock_data, on='stock_label')
 
 threshold = complete_df.stock_growth_median.describe()[5]
 top_consideration = complete_df[complete_df['stock_growth_median'] > threshold].copy()
-top_consideration = top_consideration[['stock_label', 'Nama', 'profit_growth_mean', 'profit_growth_median', 'stock_growth_mean', 'stock_growth_median']]
+top_consideration = top_consideration[['stock_label', 'Nama', 'profit_growth_mean', 'profit_growth_median', 'stock_growth_mean', 'stock_growth_median', 'last_price']]
 top_consideration.to_csv('../data/preprocessed/top_consideration/top_consideration_{0}.csv'.format(today), index=False)
 print('Top consideration at {0} successfully created.'.format(today))
