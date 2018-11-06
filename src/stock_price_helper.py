d7adc6d543e2b240934f0a8c21d4e7d02da3d49a
Reza <reza.brianca@gmail.com>
2018-11-06 21:31:50 +0700

    revise filter

--- src/stock_price_helper.py
+++ src/stock_price_helper.py
@@ -34,7 +34,7 @@ merged_data.drop(['stock'], axis=1, inplace=True)
 merged_data['diff_percent_with_avg_eps'] = round(100.0 * ((merged_data.last_price - merged_data.avg_eps_25) / merged_data.last_price), 2)
 merged_data['diff_percent_with_last_eps'] = round(100.0 * ((merged_data.last_price - merged_data.last_eps_20) / merged_data.last_price), 2)
 
-selected_data = merged_data[(merged_data.diff_percent_with_avg_eps[i] < 0.0) & (merged_data.diff_percent_with_last_eps[i] < 0.0)].copy()
+selected_data = merged_data[(merged_data.diff_percent_with_avg_eps < 0.0) & (merged_data.diff_percent_with_last_eps < 0.0)].copy()
 
 selected_data.to_csv('../data/output/final_data_{0}.csv'.format(today), index=False)
 
